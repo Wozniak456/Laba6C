6899beed50e1ea22e00575f98ecfc8632e34fd54
Sofiia <65614017+Wozniak456@users.noreply.github.com>
2020-12-09 00:47:25 +0200

    Add project files.

---
+++ ychiys/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace ychiys
+{
+    class Program
+    {
+        static double Fakt(uint number) // функція для обчислення факторіалу
+        {
+            double result = 1;
+            for (int i = 2; i <= number; i++)
+            {
+                result *= i;
+            }
+            return result;
+        }
+        static double D_result(uint n, double x, double e) //функція для обчислення суми
+        {
+            int k = 0;
+
+            int _1k = 1;
+            double kf = 1, nkf = Fakt(n), d = 0;
+
+            double x_k = _1k / kf / nkf * Math.Pow(x / 2, (2 * k + n));//значення початкового елемента ряду 
+
+            while (Math.Abs(x_k) >= e) // сумуються елементи, які задовольняють умову
+            {
+                
+                Console.WriteLine("x{1} = {0}", x_k, k);
+                d += x_k;
+                k++;
+
+                _1k *= -1;
+                kf *= k;
+                nkf *= (n + k);
+                x_k = _1k / kf / nkf * Math.Pow(x / 2, (2 * k + n));
+            }
+               Console.WriteLine("x{1} = {0}", x_k, k);
+            return d;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Input n: ");
+            uint n = Convert.ToUInt32(Console.ReadLine());
+
+            Console.Write("Input x: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Input a precision: ");
+            double e = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Sum = {0}",D_result(n, x, e));
+        }
+    }
+}
